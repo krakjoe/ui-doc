da6c527d5ba79c04dcede28f1f4604fd34e54afc
Joe Watkins <krakjoe@php.net>
2016-11-06 19:46:48 +0000

    window::msg code

---
+++ code/window/msg.php
@@ -0,0 +1,12 @@
+<?php
+use UI\Size;
+use UI\Window;
+
+$window = new Window("Title", new Size(200, 40));
+
+$window->show();
+
+$window->msg("This is the \$title parameter", "The \$msg parameter goes here");
+
+UI\run();
+?>
